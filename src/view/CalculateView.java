ef640454a4844368b15273dd0e297b7469f0848f
anna_neumoina <nav020@bk.ru>
2024-02-18 23:17:09 +0400

    Внесены изменения во view

--- src/view/CalculateView.java
+++ src/view/CalculateView.java
@@ -4,48 +4,36 @@ import controller.CalculateComplexNumbersController;
 import model.CalculateNumbers;
 import model.impl.ComplexNumbers;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.function.BiFunction;
 
 public class CalculateView {
     private final CalculateComplexNumbersController controller;
-//    private Map<String, Calculable> commands;
+    private Map<String, BiFunction<CalculateNumbers, CalculateNumbers, CalculateNumbers>> commands;
 
     public CalculateView(CalculateComplexNumbersController controller) {
         this.controller = controller;
-//        this.commands = new HashMap<>();
+        this.commands = new HashMap<>();
     }
 
     public void run(){
-//        initializeMap();
+        initializeMap();
         while (true){
             System.out.println("Введите первое комплексное число:");
             String realPart1 = prompt("Введите реальную часть первого комплексного числа");
             String imaginationPart1 = prompt("Введите мнимую часть первого комплексного числа");
-            String operation = prompt("Введите операцию: +, -, *, /");
+            String operation = prompt("Введите операцию: " + commands.keySet());
             System.out.println("Введите второе комплексное число:");
             String realPart2 = prompt("Введите реальную часть второго комплексного числа");
             String imaginationPart2 = prompt("Введите мнимую часть второго комплексного числа");
             CalculateNumbers number1 = createComplexNumbers(Double.parseDouble(realPart1), Double.parseDouble(imaginationPart1));
             CalculateNumbers number2 = createComplexNumbers(Double.parseDouble(realPart2), Double.parseDouble(imaginationPart2));
-
-            switch (operation){
-                case "+":
-                    System.out.println("Result: " + controller.sumOfElements(number1, number2));
-                    break;
-                case "-":
-                    System.out.println("Result: " + controller.differenceOfElements(number1, number2));
-                    break;
-                case "*":
-                    System.out.println("Result: " + controller.multiplyOfElements(number1, number2));
-                    break;
-                case "/":
-                    System.out.println("Result: " + controller.divisionOfElements(number1, number2));
-                    break;
-                default:
-                    System.out.println(0);
-            }
-            String countinue = prompt("Продолжаем: y/n");
-            if (countinue.equals("n")){
+            BiFunction<CalculateNumbers, CalculateNumbers, CalculateNumbers> function = commands.get(operation);
+            System.out.println("Result: " + function.apply(number1, number2));
+            String next = prompt("Продолжаем: y/n");
+            if (next.equals("n")){
                 return;
             }
         }
@@ -57,10 +45,10 @@ public class CalculateView {
         return in.nextLine();
     }
     private void initializeMap(){
-//        commands.put("+", Calculable);
-//        commands.put("-", controller.differenceOfElements(null, null));
-//        commands.put("*", controller.multiplyOfElements(null, null));
-//        commands.put("/", controller.divisionOfElements(null, null));
+        commands.put("+", controller::sumOfElements);
+        commands.put("-", controller::differenceOfElements);
+        commands.put("*", controller::multiplyOfElements);
+        commands.put("/", controller::divisionOfElements);
     }
 
     private CalculateNumbers createComplexNumbers(double real, double imagination){
